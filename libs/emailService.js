8e414a72b68f32edae296b64020cc23656897efe
mstypsan <mst@trustpilot.com>
2016-11-27 13:36:55 +0100

    make sure we save the email before sending it

--- libs/emailService.js
+++ libs/emailService.js
@@ -4,12 +4,25 @@ var async = require('async');
 var EmailMessage = require('./emailMessage');
 var sendgridService = require('./sendgridService');
 var mailgunService = require('./mailgunService');
+var emailRepository = require('./emailRepository');
 var emailProviders = [sendgridService, mailgunService];
 
 var handleEmail = function(recipient, subject, content, callback){
   var sender = process.env.SENDER_EMAIL;
   var emailMessage = new EmailMessage(sender, recipient, subject, content);
 
+  emailRepository.saveEmail(emailMessage, function(error) {
+      if(error) {
+        callback(error);
+      }
+      else {
+        sendEmail(emailMessage, callback);
+      }
+    }
+  );
+};
+
+var sendEmail = function(emailMessage, callback) {
   async.someSeries(emailProviders, sendEmailWithProvider.bind(sendEmailWithProvider, emailMessage), function (err, success) {
     if(success) {
       winston.info('Done processing e-mail id: ' + emailMessage.id);
@@ -17,7 +30,7 @@ var handleEmail = function(recipient, subject, content, callback){
     }
     else {
       //At this point the e-mail will not be sent.
-      //We need to store the e-mail data so that it is processed later.
+      //We will later need to do some retries
       winston.error('Unable to process e-mail for recipient: ' + recipient);
       callback('Not processed');
     }
